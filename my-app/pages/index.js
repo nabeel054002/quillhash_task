d1adc3272190d665aa918b4697d55b8c7a113c39
Nabeel Khan <nkhan054002@gmail.com>
2022-11-03 18:39:46 +0530

    second commit

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -6,12 +6,36 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const {ethers, providers} = require("ethers");
 import styles from '../styles/Home.module.css'
+let MAX_COUNT=5;
 
 export default function Home() {
   const [file, setFile] = useState("");
   const [url, setUrl] = useState("");
   const [lines, setLines]=useState(0);
   const [show, setShow] = useState(false);
+  const[selectedFiles,setSelectedFiles] = useState([]);
+  const [uploadedFiles, SetUploadedFiles] =useState([]);
+  const [fileLimit, setFileLimit] = useState(false);
+
+
+  const handleFileEvent = (e)=>{
+    const arr = [];
+    let count=0;
+    let files = e.target.files;
+    for(let i=0; i<files.length; i++){
+      let reader = new FileReader();
+      reader.readAsText(files[i]);
+      reader.onload = (e)=>{
+        let file = e.target.result;
+        for(let j=0; j<file.length; j++){
+          if(file[j]=='\n'){
+            count+=1;
+          }
+        }
+        setLines(count);
+      }
+    }
+  }
   //for the url i am calculating lines from the pragma solidity statement
   useEffect(()=>{
     axios.get(url).then((response)=>{
@@ -49,35 +73,36 @@ export default function Home() {
   })
   },[url])
   useEffect(()=>{
-    if(file){
-      let lines_local=0;
+    let lines_local=0;
+    uploadedFiles.forEach((file)=>{
       for(let i=0; i<file.length; i++){
         if(file[i]=='\n'){
           lines_local+=1;
         }
       }
-      setLines(lines_local);
-    }
-  }, [file])
+    })
+    setLines(lines_local);
+      
+  }, [uploadedFiles])
+
   function render(){
     if(!show){
       return (
-        <div>
+      <div>
         <div className={styles.fir}>
-        <div className={styles.first}>
-           Upload File! 
+        <input id='fileUpload' type='file' multiple 
+          onChange={handleFileEvent}
+          disabled={fileLimit}
+        />
+        <label htmlFor='fileUpload'>
+        <a  className={`btn btn-primary ${!fileLimit ? '' : 'disabled' } `}>Upload Files</a>
+        </label>
+
+        <div className="uploaded-files-list">
+          {uploadedFiles.map((idx, key) => (
+              <p key={key}>{idx.name}</p>
+          ))} 
         </div>
-          
-        <hr></hr>
-          <input className={styles.btn} type="file" name="file" onChange={(e)=>{
-            let files = e.target.files;
-            let reader = new FileReader();
-            reader.readAsText(files[0]);
-            reader.onload = (e)=>{
-              const file = (e.target.result);
-              setFile(file);
-            }
-          }}/>
         </div>
           <br></br>
         <div className={styles.second}>
